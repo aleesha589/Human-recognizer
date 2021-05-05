0122908bdbbcbaae4e0901f76d9c1c065b38408d
aleesha589 <78365337+aleesha589@users.noreply.github.com>
2021-05-05 16:52:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,28 @@ function takepic(){
     });
 }
 console.log('ml5 version is:', ml5.version);
+
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Gez-b5fh1/model.json",modelready);
+function modelready(){
+    console.log("model loaded succesfully");
+}
+
+function identify(){
+captured_img=document.getElementById("pic");
+classifier.classify(captured_img,getresults);
+
+}
+function getresults(error,result){
+if (error){
+
+console.error(error);
+}
+else{
+console.log(result);
+document.getElementById("object_name").innerHTML=result[0].label;
+document.getElementById("object_accuracy").innerHTML=result[0].confidence.toFixed(3);
+}
+
+
+}
